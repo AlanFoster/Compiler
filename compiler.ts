eefd69fe85b4212694842cae856b43cdcd90a6e1
Alan Foster <alan@alanfoster.me>
2013-03-05 21:24:21 +0000

    Add support for digits and operators and a final token result

--- compiler.ts
+++ compiler.ts
@@ -24,10 +24,12 @@ class TokenMatch {
 }
 
 class TokenHashTable {
-	constructor(key : string, pair : any) {
+		
+	constructor(key : string, value : TokenType) {
 		
 	}
 }
+
 
 
 class Tokens {
@@ -40,7 +42,7 @@ class Tokens {
 		this.addTokenMatch(TokenType.Equals, "=");
 		this.addTokenMatch(TokenType.Identifier, "identifier");
 		this.addTokenMatch(TokenType.Number, "number");
-		this.addTokenMatch(TokenType.Semicolon, "semicolon");
+		this.addTokenMatch(TokenType.Semicolon, ";");
 	}
 	
 	private addTokenMatch(tokenType : TokenType, text:string) {
@@ -79,29 +81,33 @@ class Lexer {
 			this.hashTable[(<any> TokenType)._map[i].toLowerCase()] = i;
 		};
 
-
+		var tokenPairs = [];
 		while(this.tail != undefined) {
-			var token : TokenPair = this.scan();
+			// Clear buffer
+			this.buffer = [];
 			
+			var tokenPair : TokenPair = this.scan();
 			
-			if(token == null) {
+			if(tokenPair == null) {
 				alert("Failed to match lex :: " + this.head + " " + this.tail);
 				return;
 			}
 			
+			tokenPairs.push(tokenPair);
 		}
+		
+		alert("Matching tokens are ::\n\n" + tokenPairs.join("\n\t"));
 
 		this.scan();
 	}
 
 	private peek() {
-		var nextChar = this.tail[this.pointer];
+		var nextChar = this.tail[0];
 		return nextChar;
 	}
 	
 	private consumeToBuffer() {
-		var nextChar : string = this.tail[this.pointer];
-		this.pointer++;
+		var nextChar : string = this.next();
 		this.buffer.push(nextChar);
 		
 		return nextChar;
@@ -110,22 +116,21 @@ class Lexer {
 	private next() {
 		this.head = this.tail[0];
 		this.tail = this.tail.substr(1);
+		this.pointer++;
 		
 		return this.head;
 	}
 	
 	private scan() : TokenPair {
 			// Skip whitespaces
-			if(this.peek().match(/ /)) {
-				// ignore all whitespaces
-				while(this.next().match(/ /)) {
-					alert("inside?");
-					;
-				}
+			// ignore all whitespaces
+			while(this.peek().match(/ /)) {
+				this.next();
 			}
-	
+
 			// Match reserved words => word = letter(letter|digit)*
 			if(this.peek().match(/[a-z]/i)) {
+
 				// Consume the rest
 				while(this.peek().match(/[a-z]|\d/i)) {
 					this.consumeToBuffer();
@@ -133,18 +138,43 @@ class Lexer {
 	
 				// Check if it exists in our reserved word list
 				var bufferedWord = this.buffer.join("").toLowerCase();
-	
+		
 				var matchedIndex = this.hashTable[bufferedWord];
+				// Test if we have a matching token operator/reserved word
+				// otherwise it is an idenitifer		
 				if(matchedIndex) {
 					return new TokenPair(((<any> TokenType)._map[matchedIndex]), bufferedWord);
+				} else {
+					return new TokenPair(((<any> TokenType)._map[TokenType.Identifier]), bufferedWord);
 				}
 			}
-
 			
-
+			// Operators
+			switch(this.peek()) {
+				case ';': return new TokenPair(((<any> TokenType)._map[TokenType.Semicolon]), this.next());
+				case '=': 
+					this.next();
+					if(this.peek() == '=') {
+						this.next();
+						return new TokenPair(((<any> TokenType)._map[TokenType.EqualsEquals]), "==");
+					} else {
+						return new TokenPair(((<any> TokenType)._map[TokenType.Equals]), "=");
+					}
+			}
+			
+			// Numbers
+			if(this.peek().match(/\d/)) {
+				while(this.peek().match(/\d/)) {
+					this.consumeToBuffer();
+				}
+				var entireNumber = this.buffer.toString();
+				return new TokenPair(((<any> TokenType)._map[TokenType.Number]), entireNumber);
+			}
+			
+			return undefined;
 	}
 }
 
 // Attempting to match the following string
-var testMatch = "var varIdentifier = 10;";
+var testMatch = "var foobar = 10;";
 new Lexer().lex(testMatch);
